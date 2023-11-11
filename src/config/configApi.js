dd7eeb9d680309698a81b0f53e31ad90373d6cc6
Santiya <santiya.georgieva@gmail.com>
2023-11-11 20:04:30 +0200

    delete folders

--- src/config/configApi.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleApiKey: 'AIzaSyBwyGc5nzrDvgIfFco1Is8RzszNhLw_3Lo',
-    googleSiteKey: '6LdaFQIpAAAAAMVkq-R-ibzd_XWRLqfUTAVDoUPf',
-    googleSecretApiKey: '6LdaFQIpAAAAAGT8h3NSpLXfyy5WliSqL_-fLcBU',
-    userIP: '185.45.66.70'
-};

ec0431adffe88ea0a7463249c309483320e3c6d7
Santiya <santiya.georgieva@gmail.com>
2023-12-18 13:27:28 +0200

    fix error messages in app

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
 
-function Message({ text, isVisible, onDismiss }) {
+function Message({ text, isVisible, onDismiss, error }) {
   return (
     <Alert
+      color={`${error ? 'danger' : 'success'}`}
       isOpen={isVisible}
       toggle={onDismiss}
       className="mt-3"

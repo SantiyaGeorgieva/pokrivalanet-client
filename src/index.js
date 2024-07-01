251ecf7a4b8b2f3ed80f57084a34954a485c4b9e
Santiya Georgieva <santiya.georgigeva@gmail.com>
2024-07-01 18:59:07 +0300

    add ErrorBoundary cmp for processing the global errors in the app

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { store } from "./store";
 import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <ErrorBoundary>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
